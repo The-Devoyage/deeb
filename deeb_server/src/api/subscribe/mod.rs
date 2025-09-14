324dff128ac7a99a00283d95abd83aefffa9e2ff
Nick McLean <nickisyourfan@gmail.com>
2025-09-13 22:06:57 -0500

    subscribe find many

--- deeb_server/src/api/subscribe/mod.rs
+++ deeb_server/src/api/subscribe/mod.rs
@@ -1,9 +1,42 @@
-use actix_web::{Error, HttpRequest, HttpResponse, get, rt, web};
+use actix_web::{
+    Error, HttpRequest, HttpResponse, get, rt,
+    web::{Data, Payload},
+};
 use actix_ws::AggregatedMessage;
+use deeb::{Entity, FindManyOptions, Query};
 use futures_util::StreamExt;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use crate::app_data::AppData;
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+#[serde(deny_unknown_fields)]
+pub struct SubscribeOptions {
+    entity_name: String,
+    query: Option<Query>,
+    find_many_options: Option<FindManyOptions>,
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub enum SubscribeResponseStatus {
+    Ok,
+    Error(String),
+}
+
+#[derive(Debug, Clone, Serialize)]
+pub struct SubscribeResponse {
+    status: SubscribeResponseStatus,
+    data: Option<Vec<Value>>,
+}
 
 #[get("/subscribe")]
-async fn subscribe(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+async fn subscribe(
+    req: HttpRequest,
+    stream: Payload,
+    app_data: Data<AppData>,
+) -> Result<HttpResponse, Error> {
+    let database = app_data.database.clone();
     let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
     let mut stream = stream
         .aggregate_continuations()
@@ -14,23 +47,74 @@ async fn subscribe(req: HttpRequest, stream: web::Payload) -> Result<HttpRespons
     rt::spawn(async move {
         // receive messages from websocket
         while let Some(msg) = stream.next().await {
+            let database = database.clone();
             match msg {
                 Ok(AggregatedMessage::Text(text)) => {
-                    // echo text message
-                    session.text(text).await.unwrap();
-                }
+                    let subscribe_options = match serde_json::from_str::<SubscribeOptions>(&text) {
+                        Ok(options) => options,
+                        Err(err) => {
+                            let response = SubscribeResponse {
+                                data: None,
+                                status: SubscribeResponseStatus::Error(format!(
+                                    "Error parsing JSON: {}",
+                                    err
+                                )),
+                            };
+                            session
+                                .text(serde_json::to_string(&response).unwrap())
+                                .await
+                                .unwrap();
+                            continue;
+                        }
+                    };
+                    let entity = Entity::new(&subscribe_options.entity_name);
 
-                Ok(AggregatedMessage::Binary(bin)) => {
-                    // echo binary message
-                    session.binary(bin).await.unwrap();
-                }
+                    //TODO: Handle Applied Queries && Post Query Validation!!!!
 
+                    let data = match database
+                        .deeb
+                        .find_many::<Value>(
+                            &entity,
+                            subscribe_options.query.unwrap_or(Query::All),
+                            subscribe_options.find_many_options,
+                            None,
+                        )
+                        .await
+                    {
+                        Ok(data) => data,
+                        Err(err) => {
+                            let response = SubscribeResponse {
+                                data: None,
+                                status: SubscribeResponseStatus::Error(format!(
+                                    "Error fetching data: {}",
+                                    err
+                                )),
+                            };
+                            session
+                                .text(serde_json::to_string(&response).unwrap())
+                                .await
+                                .unwrap();
+                            continue;
+                        }
+                    };
+
+                    let success_response = SubscribeResponse {
+                        data,
+                        status: SubscribeResponseStatus::Ok,
+                    };
+
+                    session
+                        .text(serde_json::to_string(&success_response).unwrap())
+                        .await
+                        .unwrap();
+                }
                 Ok(AggregatedMessage::Ping(msg)) => {
                     // respond to PING frame with PONG frame
                     session.pong(&msg).await.unwrap();
                 }
-
-                _ => {}
+                _ => {
+                    log::warn!("Unknown message type received");
+                }
             }
         }
     });
